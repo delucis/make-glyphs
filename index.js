d1926161454e7f7b95f4cbed263028565e48b31d
delucis <swithinbank@gmail.com>
2018-04-22 16:43:51 +0200

    Export .map() in index.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const LOAD_PLIST = require('load-nextstep-plist')
 const WRITE_GLYPHS = require('write-glyphs-file')
+const MAP = require('./lib/map')
 const VALIDATE = require('./lib/validate')
 const VERSION = require('./lib/version')
 
@@ -7,6 +8,7 @@ const LOAD = async fp => LOAD_PLIST(fp).then(VALIDATE)
 const WRITE = async (fp, g, o) => VALIDATE(g).then(g => WRITE_GLYPHS(fp, g, o))
 
 module.exports.load = LOAD
+module.exports.map = MAP
 module.exports.validate = VALIDATE
 module.exports.version = VERSION
 module.exports.write = WRITE
