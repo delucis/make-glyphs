55204ef33e4a83df1c1d62ff1c8d9c3aaebdaa96
delucis <swithinbank@gmail.com>
2018-04-24 12:29:29 +0200

    Export subset function in index.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const LOAD_PLIST = require('load-nextstep-plist')
 const WRITE_GLYPHS = require('write-glyphs-file')
 const MAP = require('./lib/map')
+const SUBSET = require('./lib/subset')
 const VALIDATE = require('./lib/validate')
 const VERSION = require('./lib/version')
 
@@ -9,6 +10,7 @@ const WRITE = async (fp, g, o) => VALIDATE(g).then(g => WRITE_GLYPHS(fp, g, o))
 
 module.exports.load = LOAD
 module.exports.map = MAP
+module.exports.subset = SUBSET
 module.exports.validate = VALIDATE
 module.exports.version = VERSION
 module.exports.write = WRITE
